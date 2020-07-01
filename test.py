da1c8e8690e4a2225595bf1986b35c6b2a61e0af
derks.patrick <zonkerderks@googlemail.com>
2020-07-01 19:14:12 +0200

    using new notify users module

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ from DiscordHandler import DiscordHandler
 def main():
     logger = logging.getLogger("My Application")
     logger.info("This is a Info")
-
     logger.warning("This is a Warning")
+    logger.error("This is a Error")
+    logger.debug("This is a debug")
+    logger.critical("This is a critical")
 
 
 def create_logger():
@@ -21,7 +23,8 @@ def create_logger():
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
     # Create DiscordHandler and StreamHandler
-    discord_handler = DiscordHandler(webhook_url, agent)
+    notify_users = ['2725_and_more_numbers']
+    discord_handler = DiscordHandler(webhook_url, agent, notify_users=notify_users)
     stream_handler = logging.StreamHandler()
 
     # Set log level to handlers
