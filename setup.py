c09cd34f9616f1f2fb7fbf5fb5a0e87e364ba9d0
derks.patrick <zonkerderks@googlemail.com>
2020-10-16 14:53:57 +0200

    added setup for pipy

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discord_handler",
+    version="0.0.1",
+    author="Traysercassa",
+    author_email="",
+    description="A small discord handler for the logging module using webtokens",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/traysercassa/DiscordHandler",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Logging"
+    ],
+    python_requires='>=3',
+)
